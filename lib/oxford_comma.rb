7879daaa3b2bf88bd16539c23b261ffa57592d6b
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-09 20:25:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,5 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   else
-    return array[0..-2].join(",") + " and "
+    return array[0..-2].join(",") + " and " + array[-1]
 end
